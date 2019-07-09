b55963758899fe7b131d299fa05778d9f005cf4f
Henton <wangjiahengysu@gmail.com>
2019-07-09 10:40:48 +0800

    计算两个时间戳之间有几个时间点个数不正确bug修复

--- src/main/java/tools/TimeUtil.java
+++ src/main/java/tools/TimeUtil.java
@@ -25,7 +25,8 @@ public class TimeUtil {
      * @return
      */
     public static int getResetTimes(long oldTime, long nowTime, Set<Integer> day, Set<Integer> hours) {
-        if (oldTime >= nowTime) {
+        long crossMs = nowTime - oldTime;
+        if (crossMs <= 0) {
             return 0;
         }
         int result = 0;
@@ -37,53 +38,57 @@ public class TimeUtil {
         int l = now.getHourOfDay();
         int originI = i;
         int originJ = j;
-        long crossMs = nowTime - oldTime;
         // 跨过了多少个24小时
         int crossDays = (int) (crossMs / DAY);
         // 跨过了多少周，若oldTime和nowTime同一天（如同是周一、周二）且oldTime的当前小时数大于nowTime则周数加一方便计算，后面进行修正
-        int crossWeeks = ((i == j && k > l) ? crossDays / 7 + 1 : crossDays / 7);
+        int crossWeeks = ((i == j && k >= l && crossMs >= HOUR) ? crossDays / 7 + 1 : crossDays / 7);
         // 根据跨过多少周计算有多少个指定时间点（周几几点）,因为是经过了7*24小时，因此不要考虑是周几几点，每周必定会有day.size() * hours.size()个时间点
         result += crossWeeks * day.size() * hours.size();
         // 不是同一天则追到同一天并累计时间点个数（因不一定每天都包含时间点，因此需要循环判断）
         while (i != j) {
             if (day.contains(i)) {
-                result += hours.size();
+                if(i == originI){
+                    for (int temp = k + 1; temp < 24; ++temp) {
+                        if (hours.contains(temp)) {
+                            result = result + 1;
+                        }
+                    }
+                }else {
+                    result += hours.size();
+                }
             }
             i = i % 7 + 1;
         }
         // 重置时间点不包含今天则直接返回结果
-        if (!day.contains(i) && !day.contains(originI)) {
+        if (!day.contains(j)) {
             return result;
         }
-        // 重置时间点包含今天，且oldTime的当前小时数大于nowTime，追加今天的重置次数，后边再修正
-        if (day.contains(i) && originI != originJ && k > l) {
-            result += hours.size();
-        }
-        // 开始修正
-        // oldTime的天数为重置天数，且nowTime与oldTime初始天数不同，则减去多计算的重置次数
-        if (day.contains(originI) && originI != originJ) {
-            for (int temp = 0; temp <= k; ++temp) {
+        // 重置时间点包含今天，进行修正
+        // 初始天数不同，计算0点到现在小时的重置次数即可
+        if (originI != originJ) {
+            for (int temp = 0; temp <= l; ++temp) {
                 if (hours.contains(temp)) {
-                    result = result - 1;
+                    result = result + 1;
                 }
             }
+            return result;
         }
-        // 重置时间点包含今天，进行修正
-        // 到此都在同一天如同是周一、周二）、且oldTime的当前小时数大于nowTime则之前的计算多算了n个小时的时间，需要减去
-        if (day.contains(i) && k > l) {
+        // 初始天数相同，oldTime的当前小时数小于nowTime则之前的计算少算了n个小时的时间，需要加上
+        if (k < l) {
+            // k为oldTime当前小时，是过去的时间已经计算过不需要再加
+            for (k = k + 1; k <= l; ++k) {
+                if (hours.contains(k)) {
+                    result = result + 1;
+                }
+            }
+            return result;
+        }
+        // 初始天数相同，oldTime的当前小时数大于nowTime则之前的计算多算了n个小时的时间，需要减去
+        if (k >= l) {
             // l为nowTime当前小时，当前已经达到该时间不需要减去
             for (l = l + 1; l <= k; ++l) {
                 if (hours.contains(l)) {
                     result--;
-                }
-            }
-        }
-        // oldTime的当前小时数小于nowTime则之前的计算少算了n个小时的时间，需要加上
-        if (day.contains(i) && k <= l) {
-            // k为oldTime当前小时，是过去的时间已经计算过不需要再加
-            for (k = k + 1; k <= l; ++k) {
-                if (hours.contains(k)) {
-                    result++;
                 }
             }
         }
---
+++ src/test/java/tools/TimeUtilTest.java
@@ -0,0 +1,51 @@
+package tools;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TimeUtilTest {
+
+    @Test
+    public void testGetResetTimes(){
+        // 周一
+        HashSet<Integer> day = new HashSet(Arrays.asList(1));
+        // 五点
+        HashSet<Integer> hours = new HashSet(Arrays.asList(5));
+        // 2019-02-20 16:58:01 周三  2019-03-28 16:58:01 周四
+        int resetTimes = TimeUtil.getResetTimes(1550653081000L, 1553763481000L, day, hours);
+        Assert.assertEquals(resetTimes, 5);
+        // 2019-02-20 16:58:01 周三  2019-03-25 04:59:59 周一
+        resetTimes = TimeUtil.getResetTimes(1550653081000L, 1553461199000L, day, hours);
+        Assert.assertEquals(resetTimes, 4);
+        // 2019-02-17 16:58:01 周日  2019-03-25 04:59:59 周一
+        resetTimes = TimeUtil.getResetTimes(1550393881000L, 1553461199000L, day, hours);
+        Assert.assertEquals(resetTimes, 5);
+        // 2019-02-17 16:58:01 周日  2019-03-26 04:59:59 周二
+        resetTimes = TimeUtil.getResetTimes(1550393881000L, 1553547599000L, day, hours);
+        Assert.assertEquals(resetTimes, 6);
+        // 2019-02-18 06:58:01 周一  2019-03-26 04:59:59 周二
+        resetTimes = TimeUtil.getResetTimes(1550444281000L, 1553547599000L, day, hours);
+        Assert.assertEquals(resetTimes, 5);
+        // 2019-02-18 06:58:01 周一  2019-03-26 04:59:59 周二
+        resetTimes = TimeUtil.getResetTimes(1550444281000L, 1553547599000L, day, hours);
+        Assert.assertEquals(resetTimes, 5);
+        // 2019-03-28 16:58:01 周四  2019-02-20 16:58:01 周三
+        resetTimes = TimeUtil.getResetTimes(1553763481000L, 1550653081000L, day, hours);
+        Assert.assertEquals(resetTimes, 0);
+        // 2019-02-17 16:58:01 周日  2019-02-18 16:58:01 周一
+        resetTimes = TimeUtil.getResetTimes(1550393881000L, 1550480281000L, day, hours);
+        Assert.assertEquals(resetTimes, 1);
+        // 2019-02-18 04:59:00 周一  2019-02-18 05:00:00 周一
+        resetTimes = TimeUtil.getResetTimes(1550437140000L, 1550437200000L, day, hours);
+        Assert.assertEquals(resetTimes, 1);
+        // 2019-02-18 05:00:00 周一  2019-02-18 05:00:00 周一
+        resetTimes = TimeUtil.getResetTimes(1550437200000L, 1550437200000L, day, hours);
+        Assert.assertEquals(resetTimes, 0);
+        // 2019-02-18 05:00:00 周一  2019-02-18 05:30:00 周一
+        resetTimes = TimeUtil.getResetTimes(1550437200000L, 1550439000000L, day, hours);
+        Assert.assertEquals(resetTimes, 0);
+    }
+
+}
